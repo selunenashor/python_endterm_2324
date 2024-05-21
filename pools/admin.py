d951dbf1ccde6a8c4a735081bae87d1ff00c010b
selunenashor <phong.than.kiem.771@gmail.com>
2024-05-22 03:13:44 +0700

    Test example in django.com

---
+++ pools/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from.models import Choice, Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ['question_text']}),
+        ("Date information", {"fields": ['pub_data']})
+    ]
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
